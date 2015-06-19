146acb8bbbf1b4c52c5eadc39f295acf0f28ce70
bjstabi <bstabile@42six.com>
2015-06-19 13:43:03 -0400

    Initial commit with stubbed out project

---
+++ claire-client/openfda-client/src/main/java/com/_42six/fda/openfda/OpenFDAClient.java
@@ -0,0 +1,15 @@
+package com._42six.fda.openfda;
+
+
+
+/**
+ * Hello world!
+ *
+ */
+public class OpenFDAClient 
+{
+    public OpenFDAClient() throws Exception
+    {
+    	
+    }
+}
---
+++ claire-web/src/main/java/com/_42six/fda/App.java
@@ -0,0 +1,13 @@
+package com._42six.fda;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
