535db88db5d5e279827502f04dfebda775506275
Jerry Conner <jconner@42six.com>
2015-06-23 19:06:40 -0400

    Angularize the trend information on the home page

--- claire-web/src/main/webapp/scripts/controllers/ClaireCtrl.js
+++ claire-web/src/main/webapp/scripts/controllers/ClaireCtrl.js
@@ -1,36 +1,56 @@
 'use strict';
 
-angular.module('claire.controllers').controller('ClaireCtrl', ["$scope", "$location", "DrugInfo",
-        function($scope, $location, DrugInfo) {
+angular.module('claire.controllers').controller('ClaireCtrl', ["$scope", "$location", "DrugInfo", "Trends",
+        function($scope, $location, DrugInfo, Trends) {
 
     $scope.drug = {};
+    $scope.trends = {
+        topPositive: [],
+        topNegative: [],
+        mostAdverseEvents: []
+    };
 
     var pathParts = $location.path().split("/detail/");
     var isDetailsPage = pathParts.length > 1;
-    if (isDetailsPage) {
-        $scope.drug.selected = pathParts[1];
-    }
 
     $scope.onSearch = function(drug) {
         $location.path("/detail/" + drug);
     };
 
-    $scope.$watch("drug.selected", function(newValue, oldValue) {
-        if (newValue && newValue !== oldValue) {
-            if (isDetailsPage) {
+    if (!isDetailsPage) {
+        Trends.getTopPositive(5).then(function(response) {
+            $scope.trends.topPositive = response.data;
+        });
+
+        Trends.getTopNegative(5).then(function(response) {
+            $scope.trends.topNegative = response.data;
+        });
+
+        Trends.getMostAdverseEvents(5).then(function(response) {
+            $scope.trends.mostAdverseEvents = response.data;
+        });
+    }
+
+    if (isDetailsPage) {
+        $scope.drug.selected = pathParts[1];
+    }
+
+    if (isDetailsPage) {
+        $scope.$watch("drug.selected", function(newValue, oldValue) {
+            if (newValue && newValue !== oldValue) {
                 $location.path("/detail/" + newValue);
             }
-        }
-    })
+        });
+    }
 
-    // Convert the string dates into javascript Dates
-    var transformDates = function(data) {
-        for (var i = 0; i < data.length; i++) {
-            data[i][0] = new Date(data[i][0]);
-        }
-    };
+    if (isDetailsPage) {
+        // Convert the string dates into javascript Dates
+        var transformDates = function(data) {
+            for (var i = 0; i < data.length; i++) {
+                data[i][0] = new Date(data[i][0]);
+            }
+        };
 
-    if ($scope.drug.selected) {
         DrugInfo.getTwitterStats($scope.drug.selected).then(function(response) {
             $scope.twitterStats = response.data;
         });
