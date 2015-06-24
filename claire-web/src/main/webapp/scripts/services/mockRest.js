105572405af950defddd658d6e283d924e744407
Jerry Conner <jconner@42six.com>
2015-06-24 09:07:30 -0400

    Killing the search endpoint

--- claire-web/src/main/webapp/scripts/services/mockRest.js
+++ claire-web/src/main/webapp/scripts/services/mockRest.js
@@ -66,11 +66,6 @@ angular.module('claire.mockREST', ['ngMockE2E']).run(function($injector) {
         ["enbrel","humira","aspirin","hydrochloride","tysabri","methotrexate","prednisone","metformin","avonex","lipitor","lisinopril","acetaminophen","nexium","omeprazole","avandia","simvastatin","folic","metoprolol","lyrica","mirena","synthroid","hydrochlorothiazide","dianeal","levothyroxine","remicade","lasix","furosemide","crestor","chantix","amlodipine","atenolol","seroquel","plavix","spiriva","revlimid","lantus","advair","dexamethasone","albuterol","ibuprofen","celebrex","coumadin","byetta","warfarin","insulin","prednisolone","diovan","vioxx","tylenol","cymbalta","norvasc","oxycodone","xanax","forteo","premarin","aleve","neurontin","chloride","tramadol","zoloft","fentanyl","hydrocodone","gabapentin","morphine","tartrate","paxil","fosamax","prilosec","allopurinol","cyclophosphamide","niaspan","letairis","wellbutrin","ribavirin","acetylsalicylic","effexor","zocor","lorazepa"]
     );
 
-    /**
-     * Search
-     */
-    $httpBackend.whenGET(/api\/drugs\/test/).respond(['test1', 'test2']);
-
 
     /**
      * Trends
